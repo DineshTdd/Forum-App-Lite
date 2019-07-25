2a415fa102a655923eacfc91aab694774a20da98
Dinesh Thirumaran <dineshthiru.1998@gmail.com>
2019-07-25 19:47:24 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('../controllers/user');
 
 const router = express.Router();
+console.log("hi from user route");
 
 router.post('/api/user/signup', userController.createUser);
 
